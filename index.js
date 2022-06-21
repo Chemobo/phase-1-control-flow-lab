536d4254b45d9dd7329c4cd9ca71edf42054941b
James Chemobo <jkchemobo@gmail.com>
2022-06-21 10:24:47 +0300

    commit tree 7cb6c291701aee3c8bf1f83cf708e5d440203595

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+
+  let scuberGreetingForFeet
+
+ if (someValue <=400) {
+  scuberGreetingForFeet = 'This one is on me!';
+  return scuberGreetingForFeet
+ } else if (someValue >2000 && someValue <2500 ) {
+  scuberGreetingForFeet = 'I will gladly take your thirty bucks.';
+ return scuberGreetingForFeet
+} else if (someValue >2500) {
+  scuberGreetingForFeet = 'No can do.'
+  return scuberGreetingForFeet
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city==='NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip==='generous'){
+    return 'Thank you so much.'
+  }
+  else if(tip=== 'not as generous'){
+    return 'Thank you.';
+
+  } else {
+    return 'Bye.';
+  }
 }
